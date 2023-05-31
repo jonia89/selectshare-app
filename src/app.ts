58fb39f88af33e9393a809c46091a2f7e9d3ee88
jonia89 <jonianttila89@hotmail.com>
2023-05-31 18:30:04 +0300

    find a place app

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const googleApiKey = process.env.Google_API_Key;
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+//declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${googleApiKey}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 18,
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+require("dotenv").config();
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new Dotenv(),],
+};
